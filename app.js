c7a9d658b058e4baef2a9f7065cbb6a205d0f4bb
AELMOUDEN <ahmed_elmouden3@um5.ac.ma>
2023-05-21 13:31:02 +0100

    added POST /order endpoint

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ const jwt = require("jsonwebtoken");
 const LocalStrategy = require("passport-local");
 const MongoStore = require("connect-mongo");
 const nodemailer = require("nodemailer");
-const Product = require('./models/product');
+const Product = require("./models/product");
 
 // Seeder. Remove when run for the first time
 //require("./seed");
 
 const dbURI = process.env.MONGODBURI || "mongodb://127.0.0.1:27017/agileDB";
-
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
@@ -28,7 +27,6 @@ db.on("error", console.error.bind(console, "Connection Error:"));
 db.once("open", () => {
   console.log("Database Connected");
 });
-
 
 app.use((req, res, next) => {
   console.log("req.session: ", req.session);
@@ -43,14 +41,33 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/orders", loggedIn, async (req, res) => {
-    const clients = await Client.find({});
-    res.render("orders", { clients });
-  });
+  const clients = await Client.find({});
+  res.render("orders", { clients });
+});
 
+app.post("/order", async (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  const {
+    color: orderedProduct,
+    first_name: fullName,
+    city,
+    phone: phoneNumber,
+    address: shippingAddress,
+  } = req.body;
+  const client = {
+    fullName,
+    city,
+    phoneNumber,
+    shippingAddress,
+    orderedProduct,
+  };
 
+  const newClient = new Client(client);
+  await newClient.save();
 
-
-
+  res.sendStatus(200);
+});
 
 // use env variable to define tcp/ip port with a default
 const PORT = process.env.PORT || 3000;
