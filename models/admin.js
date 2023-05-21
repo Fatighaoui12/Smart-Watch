f28b5174551a1aa8f55944c16c0d80fed115440b
Fatighaoui12 <104173820+Fatighaoui12@users.noreply.github.com>
2023-05-21 14:06:05 +0100

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+
 const Admin = new Schema({
     email: {type: String, required:true, unique:true},
     username : {type: String, unique: true, required:true}
